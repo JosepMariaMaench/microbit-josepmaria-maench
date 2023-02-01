f347990a2606b8754939aeae37f6dbc7cd9e9806
JosepMariaMaench <98151393+JosepMariaMaench@users.noreply.github.com>
2023-02-01 19:29:46 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,64 @@
+// Es mostren els numeros quadrats fins al maxim numero de atimes
+let i = 0
+let bclicked = 0
+let atimes = 0
+function interact(interval: number) {
+    
+    basic.clearScreen()
+    i = 1
+    while (i < interval + 1) {
+        basic.showString("" + ("" + operacio(i)))
+        basic.pause(100)
+        basic.clearScreen()
+        i += 1
+    }
+}
+
+function operacio(n: number): number {
+    return Math.pow(n, 2)
+}
+
+function numElements() {
+    
+    while (bclicked < 1) {
+        if (input.buttonIsPressed(Button.A)) {
+            basic.showString("A")
+            basic.clearScreen()
+            atimes += 1
+            basic.showString("" + ("" + atimes))
+        } else if (input.buttonIsPressed(Button.B)) {
+            basic.showString("B")
+            bclicked += 1
+        }
+        
+        basic.pause(50)
+    }
+    basic.clearScreen()
+    basic.showString("Suma1")
+    interact(atimes)
+}
+
+function showIcon() {
+    basic.clearScreen()
+    music.startMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once)
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.SmallDiamond)
+        basic.pause(100)
+        basic.showIcon(IconNames.Diamond)
+        basic.pause(100)
+    }
+    basic.clearScreen()
+    basic.showString("Suma 1")
+    basic.clearScreen()
+}
+
+basic.forever(function on_forever() {
+    
+    music.setBuiltInSpeakerEnabled(true)
+    atimes = 0
+    bclicked = 0
+    showIcon()
+    basic.showString("Numeros quadrats")
+    numElements()
+    basic.pause(500)
+})
